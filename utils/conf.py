263a3bdb909c1f058358176ebf5f70cbd60f5dfb
社亦园 <2125107118@qq.com>
2025-11-21 10:18:17 +0800

    feat: 优化配置文件为更易读的形式

--- utils/conf.py
+++ utils/conf.py
@@ -16,12 +16,12 @@ class Config:
             os.makedirs(self.path)
         if not os.path.exists(os.path.join(self.path,self.file)):
             with open(os.path.join(self.path,self.file),"w") as f:
-                json.dump(self.conf,f)
+                json.dump(self.conf, f, indent=2, ensure_ascii=False)
         with open(os.path.join(self.path,self.file)) as f:
             self.conf = json.load(f)
     def save(self):
         with open(os.path.join(self.path,self.file),"w") as f:
-            json.dump(self.conf,f)
+            json.dump(self.conf, f, indent=2, ensure_ascii=False)
     def get(self):
         return self.conf
     def set(self,conf:dict):
